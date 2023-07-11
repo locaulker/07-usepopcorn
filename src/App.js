b7697d6d1b5d7cb4a1192626830c2e05b29b02c6
Lawrence Caulker <locaulker@gmail.com>
2023-07-10 22:20:38 -0400

    resume L152 @11:47

--- src/App.js
+++ src/App.js
@@ -262,6 +262,7 @@ function Movie({ movie, onSelectMovie }) {
 function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
   const [movie, setMovie] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+  const [userRating, setUserRating] = useState("")
 
   const {
     Title: title,
@@ -331,7 +332,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
           <section>
             <div className="rating">
-              <StarRating maxRating={10} size={24} />
+              <StarRating
+                maxRating={10}
+                size={24}
+                onSetRating={setUserRating}
+              />
 
               <button className="btn-add" onClick={handleAdd}>
                 + Add to list
