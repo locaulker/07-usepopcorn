68b1ef71f28f4970e54cf1a6e0933666d5c2376d
Lawrence Caulker <locaulker@gmail.com>
2023-07-17 19:47:18 -0400

    end L167 - using refs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import StarRating from "./StarRating"
 
 const average = arr =>
@@ -7,7 +7,7 @@ const average = arr =>
 const KEY = "f5ddaba"
 
 export default function App() {
-  const [query, setQuery] = useState("shutter")
+  const [query, setQuery] = useState("")
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState("")
@@ -161,6 +161,32 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null)
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return
+
+        if (e.code === "Enter") {
+          inputEl.current.focus()
+          setQuery("")
+        }
+      }
+
+      document.addEventListener("keydown", callback)
+
+      return () => document.addEventListener("keydown", callback)
+    },
+    [setQuery]
+  )
+
+  // useEffect(function () {
+  //   const el = document.querySelector(".search")
+  //   console.log(el)
+  //   el.focus()
+  // }, [])
+
   return (
     <input
       className="search"
@@ -168,6 +194,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={e => setQuery(e.target.value)}
+      ref={inputEl}
     />
   )
 }
